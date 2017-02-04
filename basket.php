ad95ae4635a2a7be8df1945f0b2e203d1de5325e
Gayashan Bombuwala <gayashanbc@gmail.com>
2017-02-04 23:29:11 +0530

    Added some CSS styles to the page

--- basket.php
+++ basket.php
@@ -2,6 +2,10 @@
 //create a variable called $pagename which contains the actual name of the page
 $pagename = "Basket";
 
+//Google Web Fonts
+echo "<link href='//fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>";
+echo "<link href='//fonts.googleapis.com/css?family=Exo 2' rel='stylesheet'>";
+
 //call in the style sheet called ystylesheet.css to format the page as defined in the style sheet
 echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
 
@@ -14,7 +18,7 @@ include("headfile.html");
 echo "<p></p>";
 //display name of the page and some random text
 echo "<h2>" . $pagename . "</h2>";
-echo "<p> Text Here";
+echo "<p> Text Here</p>";
 
 //include head layout
 include("footfile.html");
