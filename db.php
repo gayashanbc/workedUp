04373259ed0a8a5368639a5c22c7263af7fb4870
Gayashan Bombuwala <gayashanbc@gmail.com>
2017-02-20 15:31:16 +0530

    Added db.php and modified index.php

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'cybertec_basic';
+$dbpass = 'ID961160367';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if (!$conn) {
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db("cybertec_hexis", $conn);
+?>
